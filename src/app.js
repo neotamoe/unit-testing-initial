5c9030ae64e7498567d9a4a3db84afdcd2dd0616
Neota Moe <neota.moe@gmail.com>
2017-05-24 14:40:33 -0500

    added secondparseInt

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ var addOne = require('./modules/addOne');
 app.get('/addOne/:number', function(req,res){
   console.log('inside GET route', req.params.number);
   // note that number may be a string
-  var myNumber = req.params.number;
+  var myNumber = parseInt(req.params.number);
   var result = addOne(myNumber);
   console.log('result:', result);
   res.status(200).send({answer: result});
